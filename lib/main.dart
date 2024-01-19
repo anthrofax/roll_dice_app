76f5fb1eb51b2eb7afb459179d5f89a3ed226c03
anthrofax <afridhoikhsan@gmail.com>
2024-01-19 23:06:11 +0700

    Implementing Navigation, Routes in Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,170 +1,62 @@
-// 1.
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp( MaterialApp(
-//     home:  HalamanSatu(),
-//   ));
-// }
-
-// class HalamanSatu extends StatelessWidget {
-//   @override
-
-//   Widget build (BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: Colors.black,
-//         leading: const Icon(Icons.home, color: Colors.white, size: 30.0,),
-//         title: const Center(
-//           child:  Text('Anthrofax', style: TextStyle(color: Colors.white))
-//         ),
-//         actions: const [
-//            Icon(Icons.search, color: Colors.white, size: 30.0,)
-//         ],
-//       ),
-//       body: Center(
-//         child: Container(
-//           color: Colors.black38,
-//           width: 200.0,
-//           height: 100.0,
-//           child: Icon(Icons.android, color: Colors.yellow[50], size: 70.0,),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// 2. Column & Row (Layout Setting)
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(MaterialApp(
-//     home: HalamanSatu(),
-//   ));
-// }
-
-// class HalamanSatu extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         appBar: AppBar(
-//           backgroundColor: Colors.black,
-//           leading: const Icon(
-//             Icons.home,
-//             color: Colors.white,
-//             size: 30.0,
-//           ),
-//           title: const Center(
-//               child: Text('Anthrofax', style: TextStyle(color: Colors.white))),
-//           actions: const [
-//             Icon(
-//               Icons.search,
-//               color: Colors.white,
-//               size: 30.0,
-//             )
-//           ],
-//         ),
-//         body: Column(
-//           children: [
-//             Icon(
-//               Icons.donut_large,
-//               color: Colors.red[200],
-//               size: 70.0,
-//             ),
-//             Row(
-//               children: [
-//                 Icon(
-//                   Icons.local_pizza,
-//                   color: Colors.red[200],
-//                   size: 70.0,
-//                 ),
-//                 const Icon(
-//                    Icons.local_pizza,
-//                   color: Colors.blue,
-//                   size: 70.0,
-//                 ),
-//                 Icon(
-//                   Icons.local_pizza,
-//                   color: Colors.red[200],
-//                   size: 70.0,
-//                 ),
-//               ],
-//             ),
-//             const Icon(
-//               Icons.donut_large,
-//               color: Colors.blue,
-//               size: 70.0,
-//             )
-//           ],
-//         ));
-//   }
-// }
-
-// 3. Parsing Data
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: HalamanSatu(),
+    home: const HalamanSatu(),
+    title: 'Latihan Navigasi',
+    routes: <String, WidgetBuilder>{
+      '/HalamanSatu': (BuildContext context) => const HalamanSatu(),
+      '/HalamanDua': (BuildContext context) => const HalamanDua(),
+    },
   ));
 }
 
 class HalamanSatu extends StatelessWidget {
+  const HalamanSatu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue[50],
+          title: const Center(
+            child: Text('Halaman Satu'),
+          ),
+        ),
+        body: Center(
+          child: IconButton(
+            icon: Icon(
+              Icons.android,
+              color: Colors.green[300],
+              size: 50.0,
+            ),
+            onPressed: () {
+              Navigator.pushNamed(context, '/HalamanDua');
+            },
+          ),
+        ));
+  }
+}
+
+class HalamanDua extends StatelessWidget {
+  const HalamanDua({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.black,
-        leading: const Icon(
-          Icons.home,
-          color: Colors.white,
-          size: 30.0,
-        ),
+        backgroundColor: Colors.blue[50],
         title: const Center(
-            child: Text('Anthrofax', style: TextStyle(color: Colors.white))),
-        actions: const [
-          Icon(
-            Icons.search,
-            color: Colors.white,
-            size: 30.0,
-          )
-        ],
+          child: Text('Halaman Dua'),
+        ),
       ),
       body: Center(
-          child: Column(
-        children: [
-          Card(icon: Icons.home, text: "Home", color: Colors.grey),
-          Card(icon: Icons.favorite, text: "Favorite", color: Colors.pink),
-          Card(icon: Icons.place, text: "Place", color: Colors.blue),
-          Card(icon: Icons.settings, text: "Settings", color: Colors.black),
-        ],
-      )),
-    );
-  }
-}
-
-class Card extends StatelessWidget {
-  @override
-  Card({required this.icon, required this.text, required this.color});
-
-  final IconData icon;
-  final String text;
-  final Color color;
-
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(10.0),
-      color: Colors.grey[300],
-      child: Column(
-        children: [
-          Icon(
-            icon,
-            color: color,
-            size: 50.0,
-          ),
-          Text(text)
-        ],
-      ),
+          child: IconButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/HalamanSatu');
+              },
+              icon: const Icon(Icons.music_note,
+                  color: Colors.black, size: 50.0))),
     );
   }
 }
