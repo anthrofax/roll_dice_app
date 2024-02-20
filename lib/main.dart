d261090b4e45910982cfe8669b9b67cf6d0a1857
anthrofax <afridhoikhsan@gmail.com>
2024-02-20 07:59:09 +0700

    Extracting code logic into a seperate widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,27 @@ void main() {
   runApp(
     MaterialApp(
         home: Scaffold(
-      backgroundColor: const Color.fromARGB(255, 184, 163, 87),
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-              colors: [Colors.black, Colors.orange],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight),
-        ),
-        child: const Center(
-          child: Text(
-            'Hello Flutter',
-            style: TextStyle(color: Colors.white, fontSize: 28),
-          ),
+            backgroundColor: const Color.fromARGB(255, 184, 163, 87),
+            body: GradientContainer())),
+  );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            colors: [Colors.black, Colors.orange],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello Flutter',
+          style: TextStyle(color: Colors.white, fontSize: 28),
         ),
       ),
-    )),
-  );
+    );
+  }
 }
