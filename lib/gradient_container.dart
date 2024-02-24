4f1236e7ede2d04754e0d71c75dbe0d575cd687a
anthrofax <afridhoikhsan@gmail.com>
2024-02-24 23:52:00 +0700

    Giving text button some style (text button color, font size of text button & Giving space between dice image and text button using Sized Box

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -28,16 +28,26 @@ class GradientContainer extends StatelessWidget {
           gradient: LinearGradient(
               colors: colors, begin: startAlignment, end: endAlignment),
         ),
-        child: Column(
-          children: [
-            Center(
-              child: Image.asset(
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset(
                 'assets/img/dice-1.png',
                 width: 200,
               ),
-            ),
-            TextButton(onPressed: rollDice, child: const Text('Roll Dice'))
-          ],
+              const SizedBox(
+                height: 20,
+              ),
+              TextButton(
+                  onPressed: rollDice,
+                  style: TextButton.styleFrom(
+                      // padding: const EdgeInsets.only(top: 20),
+                      foregroundColor: Colors.white,
+                      textStyle: const TextStyle(fontSize: 28)),
+                  child: const Text('Roll Dice'))
+            ],
+          ),
         ));
   }
 }
